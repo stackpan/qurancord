37695d4ba874cfdfb204aac199645ce83c94e412
Ivan <ivanrizqy.ir@gmail.com>
2023-12-27 18:57:28 +0700

    Initial commit to using spring boot

---
+++ src/main/java/com/ivanzkyanto/qcv2/Qcv2Application.java
@@ -0,0 +1,13 @@
+package com.ivanzkyanto.qcv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qcv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Qcv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivanzkyanto/qcv2/Qcv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanzkyanto.qcv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qcv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
